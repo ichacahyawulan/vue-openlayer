3a55475a48c2794d9a189276e76e68ad273a892c
Icha Cahya Wulan <ichacahyawulan@gmail.com>
2021-08-10 01:50:27 +0700

    vue openlayer

---
+++ src/components/Map.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="app">
+        <h2>Map using openlayer and vue</h2>
+        <div id="map"></div>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+// import openlayer css for style
+import "ol/ol.css";
+// This is library of openlayer for handle map
+import Map from "ol/Map";
+import View from "ol/View";
+import { defaults as defaultControls, ScaleLine } from "ol/control";
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {OSM, Vector as VectorSource} from 'ol/source';
+
+export default {
+    async mounted() {
+        await this.initiateMap();
+    },
+    methods: {
+        initiateMap() {
+        // create vector layer
+        var source = new VectorSource();
+        var vector = new VectorLayer({
+            source: source
+        });
+        // create title layer
+        var raster = new TileLayer({
+            source: new OSM(),
+        });
+        // create map with 2 layer
+        var map = new Map({
+            // controls: defaultControls().extend([
+            // new ScaleLine({
+            //     units: "degrees",
+            // }),
+            // ]),
+            target: "map",
+            layers: [raster, vector],
+            view: new View({
+            projection: "EPSG:4326",
+            center: [107.60, -6.93],
+            zoom: 12,
+            }),
+        });
+        },
+    },
+};
+</script>
+
+<style>
+#map {
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    height: 500px;
+    width: 99%;
+}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+#nav {
+    padding: 30px;
+}
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+    color: #42b983;
+}
+</style>
